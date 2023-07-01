f2e17def2cee8062bc7be7270d6bfd783e16a74e
salitog <salvador.gutierrez2k@me.com>
2023-07-01 03:02:32 -0500

    Working on scrolling

--- funFunc.js
+++ funFunc.js
@@ -1,10 +1,35 @@
-window.onscroll = function() {
-    let currentScroll = window.pageYOffset;
-    let middleText = document.querySelector('.middle-text');
+let startY = 0;
+
+let middleText = document.querySelector(".middle-text");
+
+window.addEventListener('touchstart', function(event) {
+  startY = event.touches[0].clientY;
+});
+
+window.addEventListener('touchmove', function(event) {
+  let currentY = event.touches[0].clientY;
+  let scrollDirection = (currentY > startY) ? 'down' : 'up';
+
+  console.log('Scrolling direction:', scrollDirection);
+  middleText.innerHTML = scrollDirection;
+
+  // Add your custom logic here based on the scroll direction
+
+  event.preventDefault();
+});
+
   
-    if (currentScroll > 0) {
-      middleText.innerHTML = "Check out";
-    } else {
-      middleText.innerHTML = "Hola :)";
-    }
-  };
+//   // document.body.classList.remove('no-scroll');
+
+let previousScrollPosition = 0;
+
+window.addEventListener('wheel', function(event) {
+  // event.preventDefault(); // Prevent default scrolling behavior
+
+  let currentScrollPosition = previousScrollPosition + event.deltaY;
+  window.scrollTo(0, 0);
+  let scrollDirection = (currentScrollPosition > previousScrollPosition) ? 'down' : 'up';
+  middleText.innerHTML = scrollDirection;
+
+  previousScrollPosition = currentScrollPosition;
+});
